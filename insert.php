5c9b03f0afc1fe0c1518692dce5f04ef9fee8503
Hatim Raipurwala <hatimhbr@gmail.com>
2019-09-11 16:01:28 +0530

    commit 11/09

--- insert.php
+++ insert.php
@@ -1,49 +1,41 @@
 <?php
-$name = $_POST['name'];
+$username = $_POST['name'];
 $email = $_POST['email'];
-$subject = $_POST['subject'];
+$phone = $_POST['phone'];
 $message = $_POST['message'];
-
-if(!empty($name) || !empty($email) || !empty($subject) || !empty($message))
-{
-    $host = "localhost";
-    $dbUsername = "hatimhr";
-    $dbPassword = "hatimhr8704";
-    $dbname = "contact_form";
-
-    //creating connection
-    $conn = new mysqli($host,$dbUsername,$dbPassword,$dbname);
-
-    if(mysqli_connect_error()) {
-        die('Connect Error('.mysqli_connect_errno().')'. mysqli_connect_error());
-    } else {
-       $SELECT = "SELECT email From contact_form Where email = ? Limit 1";
-       $INSERT = "INSERT Into contact_form (name, email, subject, message) values (?, ?, ?, ?,)";
-       
-       //prepare statement
-       $stmt = $conn->prepare($SELECT);
-       $stmt ->bind_param("s", $email);
-       $stmt->execute();
-       $stmt->bind_result($email);
-       $stmt->store_result();
-       $rnum = $stmt->num_rows;
-
-       if($rnum==0) {
-           $stmt->close();
-
-           $stmt = $conn->prepare($INSERT);
-           $stmt->bind_param("ssss",$name,$email,$subject,$message);
-           $stmt->execute();
-           echo "New record inserted successfully";
-       } else {
-           echo "Someone already register using this email";
-       }
-       $stmt->close();
-       $conn->close();
-       }
-    }
- {
-    echo "All fields are required";
-    die();
-}
-?>
+if (!empty($username) || !empty($email) || !empty($phone) || !empty($message)) {
+	 $host = "localhost";
+	 $dbUsername = "root";
+	 $dbPassword = "Welcome01";
+	 $dbname = "youtube";
+	     //create connection
+	 $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+	 if (mysqli_connect_error()) {
+	 die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+	 } else {
+	 $SELECT = "SELECT email From register Where email = ? Limit 1";
+	 $INSERT = "INSERT Into register (username, email, phone, message) values(?, ?, ?, ?)";
+	 ////Prepare statement
+	 $stmt = $conn->prepare($SELECT);
+	 $stmt->bind_param("s", $email);
+	 $stmt->execute();
+	 $stmt->bind_result($email);
+	 $stmt->store_result();
+	 $rnum = $stmt->num_rows;
+	 if ($rnum==0) {
+	 $stmt->close();
+	 $stmt = $conn->prepare($INSERT);
+	 $stmt->bind_param("ssis", $username, $email, $phone, $message);
+	 $stmt->execute();
+	 echo "New record inserted sucessfully";
+	 } else {
+	 echo "Someone already register using this email";
+	 }
+	 $stmt->close();
+	 $conn->close();
+	     }
+	     } else {
+	     echo "All field are required";
+	      die();
+	      }
+	      ?>
